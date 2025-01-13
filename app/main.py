ea99c13d2d26f6c8b8b45f45fcc73bd3c9192cb6
jst-tsushima <index0205@gmail.com>
2025-01-13 15:24:58 +0900

    add: roleを指定したchat モデル

--- app/main.py
+++ app/main.py
@@ -26,3 +26,61 @@ def read_root():
     model = ChatOpenAI(model="gpt-3.5-turbo")
     res = model.invoke("Hello, World")
     return res
+
+@app.get("/lang-trans")
+def read_root():
+    from langchain_core.messages import HumanMessage, SystemMessage
+    # role指定
+    messages = [
+        SystemMessage("以下の言葉を日本語から英語に翻訳してください。"),
+        HumanMessage("今日の朝ごはんは、納豆と味噌汁と卵焼きでした。")
+    ]
+    model = ChatOpenAI(model="gpt-3.5-turbo")
+    res = model.invoke(messages)
+    return res
+    # {
+    #     "content": "This morning's breakfast was natto, miso soup, and tamagoyaki.",
+    #     "additional_kwargs": {
+    #         "refusal": null
+    #     },
+    #     "response_metadata": {
+    #         "token_usage": {
+    #             "completion_tokens": 19,
+    #             "prompt_tokens": 66,
+    #             "total_tokens": 85,
+    #             "completion_tokens_details": {
+    #                 "accepted_prediction_tokens": 0,
+    #                 "audio_tokens": 0,
+    #                 "reasoning_tokens": 0,
+    #                 "rejected_prediction_tokens": 0
+    #             },
+    #             "prompt_tokens_details": {
+    #                 "audio_tokens": 0,
+    #                 "cached_tokens": 0
+    #             }
+    #         },
+    #         "model_name": "gpt-3.5-turbo-0125",
+    #         "system_fingerprint": null,
+    #         "finish_reason": "stop",
+    #         "logprobs": null
+    #     },
+    #     "type": "ai",
+    #     "name": null,
+    #     "id": "run-fa2b23ad-9f54-45ac-acd9-78290cacccf9-0",
+    #     "example": false,
+    #     "tool_calls": [],
+    #     "invalid_tool_calls": [],
+    #     "usage_metadata": {
+    #         "input_tokens": 66,
+    #         "output_tokens": 19,
+    #         "total_tokens": 85,
+    #         "input_token_details": {
+    #             "audio": 0,
+    #             "cache_read": 0
+    #         },
+    #         "output_token_details": {
+    #             "audio": 0,
+    #             "reasoning": 0
+    #         }
+    #     }
+    # }
